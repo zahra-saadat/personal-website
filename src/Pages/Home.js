5211845e47e6da047a0b0a461eec18a359539e44
Zahra SAADATFARD <150349127+zahra-saadat@users.noreply.github.com>
2024-06-12 20:05:06 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,11 +14,9 @@ const Home = () => {
                 Click here to add your own text and edit me. 
                 I’m a great place for you to tell a story 
                 and let your users know a little more about you.</p>
-            </div>
-            <div className="btn-group btn-group-lg pb-0">
-                <button type="button" className="btn btn-warning">Resume</button>
-                <button type="button" className="btn btn-warning">Projects</button>
-                <button type="button" className="btn btn-warning">Contact</button>
+                <button type="button" className="btn btn-danger btn-lg me-1">Resume</button>
+                <button type="button" className="btn btn-warning btn-lg text-white me-1">Projects</button>
+                <button type="button" className="btn btn-success btn-lg">Contact</button>
                 
             </div>
         </div>
