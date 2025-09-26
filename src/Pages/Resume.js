dd63d84c0e1e4e49a30dab8b6643630ead6c23e2
Zahra SAADATFARD <150349127+zahra-saadat@users.noreply.github.com>
2025-09-26 13:59:12 +0200

    Update Resume.js

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -34,7 +34,7 @@ const Resume = () => {
                            Click here to add your own text and edit me. 
                            It’s easy. Just click “Edit Text” or double click me 
                            to add your own content 
-                           and make changes to the font. </p>
+                           and make changes to the font. </p> 
                         </div>
                      </div>
                   </div>
