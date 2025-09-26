9b03fe55434274158b675aa254ae1bfa2f0d1d4c
Zahra SAADATFARD <150349127+zahra-saadat@users.noreply.github.com>
2025-09-26 13:41:52 +0200

    developing page of Resume

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,4 +1,36 @@
 const Resume = () => {
- return <div></div>
+ return <div className="container">
+         <div className="row mt-5 pb-5">
+            <div className="col-sm-6 mt-5">
+               <div className="mt-5 ms-5 ps-5 pt-5">
+                <span className="h1 mt-5">Resume</span>
+                </div>
+                <div className="ms-5 ps-5 pt-5">
+                  <span className="h4">Work Experience</span>
+                </div>
+            </div>
+            <div className="col-sm-6 mt-5">
+               <div className="row mt-5 pb-5">
+                  <div className="col-sm-3 mt-5">
+                     <div className="mt-5 ps-5 pt-5 float-end">
+                        <span className="">2010-2015</span>
+                     </div>
+                  </div>
+                  <div className="col-sm-6 mt-5">
+                     <div className="mt-5 ps-1 pt-5">
+                        <span className="h5">Developer</span>
+                     </div>
+                     <div className="mt-1 ps-1 pt-1">
+                        <p>I'm a paragraph. 
+                           Click here to add your own text and edit me. 
+                           It’s easy. Just click “Edit Text” or double click me 
+                           to add your own content 
+                           and make changes to the font. </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>    
+    </div>
 }
 export default Resume
